91f437fd613f46bb4a173a67c8a76106fa0f2cd0
SeanSM-NAB <SeanSMaseola@gmail.com>
2025-12-29 21:55:11 +0200

    ...

--- electron/preload.js
+++ electron/preload.js
@@ -3,4 +3,7 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electronAPI", {
   //Create Task Function
   createTask: (newTask) => ipcRenderer.invoke("create-task", newTask),
+
+  //Read Rask Function
+  readTask: () => ipcReaderer.invoke("read-items"),
 });
